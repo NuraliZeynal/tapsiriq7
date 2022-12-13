f6f9123f4ee5129fe4ceee5b2841e2605ce33fd6
nuralizeynal <nurelizeynalli@gmail.com>
2022-12-13 11:32:15 +0300

    seko

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./styles.css";
+class App extends Component {
+  state = {
+    name: "",
+    surname: "",
+    phone: "",
+    email: "",
+    feed: ""
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  changeHandler = (e) => {
+    if (e.target.id === "name") {
+      this.setState({ name: e.target.value });
+    }
+    if (e.target.id === "surname") {
+      this.setState({ surname: e.target.value });
+    }
+    if (e.target.id === "phone") {
+      this.setState({ phone: e.target.value });
+    }
+    if (e.target.id === "email") {
+      this.setState({ email: e.target.value });
+    }
+    if (e.target.id === "feed") {
+      this.setState({ feed: e.target.value });
+    }
+  };
+
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+    alert("Qebul edilmisdir");
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="name">
+            Adiniz: <br />
+            <input
+              id="name"
+              type="text"
+              value={this.state.name}
+              onChange={this.changeHandler}
+            />
+          </label>
+          <br />
+          <label htmlFor="surname">
+            Soyadiniz: <br />
+            <input
+              id="surname"
+              type="text"
+              value={this.state.surname}
+              onChange={this.changeHandler}
+            />
+          </label>
+          <br />
+
+          <label htmlFor="email">
+            Poct unvaniniz:
+            <br />
+            <input
+              id="email"
+              type="email"
+              value={this.state.email}
+              onChange={this.changeHandler}
+            />
+          </label>
+          <br />
+
+          <label htmlFor="phone">
+            Elaqe nomreniz:
+            <br />
+            <input
+              id="phone"
+              type="text"
+              value={this.state.phone}
+              onChange={this.changeHandler}
+            />
+          </label>
+          <br />
+
+          <label htmlFor="feed">
+            Qeyd sahesi:
+            <br />
+            <textarea
+              id="serh"
+              type="text"
+              value={this.state.feed}
+              onChange={this.changeHandler}
+            />
+          </label>
+          <br />
+          <button type="submit">Gonder</button>
+        </form>
+      </div>
+    );
+  }
 }
-
 export default App;
